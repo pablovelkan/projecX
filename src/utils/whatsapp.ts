c3e40ef82ed7fc91676f77ce116e3659287f08c5
Pablo Ferreira <pablo.velkan.platao@gmail.com>
2026-06-01 16:53:56 -0300

    Atualiza link do WhatsApp para o novo grupo

--- src/utils/whatsapp.ts
+++ src/utils/whatsapp.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_GROUP_LINK = "https://chat.whatsapp.com/KNaNMRgJIsn6bB1q67oqft";
+export const WHATSAPP_GROUP_LINK = "https://chat.whatsapp.com/K0jOFcl2xGZ29bS03tIIm0";
 
 export function whatsappLink() {
   return WHATSAPP_GROUP_LINK;
